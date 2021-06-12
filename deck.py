02fc8ee8e4cdab4d1cc46f05b6db589123a80961
Cocco_Alberto <cocco.alberto97@gmail.com>
2021-06-12 14:00:03 +0200

    test //

--- deck.py
+++ deck.py
@@ -72,8 +72,7 @@ class Deck(object):
 
         self.logger.info("Giocatori attualmente connessi %d", players.count)
         # ogni 10 giocatori aggiungo un masso
-        decks: int = 1 
-        #int(operator.truediv(players.count, 10)) + 1
+        decks: int = int(players.count//10) + 1
         self.cards.clear()
 
         self.logger.info("Giochiamo con %d mazzi", decks)
